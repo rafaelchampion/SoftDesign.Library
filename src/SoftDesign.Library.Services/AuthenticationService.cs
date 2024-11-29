aadd1d13969eb80f006d0271db3ba428bbe1c1b5
Refael Ferreira <rafael.ferreira@g10x.com.br>
2024-11-29 16:58:06 -0300

    Implementação de serviços

--- src/SoftDesign.Library.Services/AuthenticationService.cs
+++ src/SoftDesign.Library.Services/AuthenticationService.cs
@@ -1,7 +1,29 @@
+using SoftDesign.Library.Cross.Core.Results;
+using SoftDesign.Library.Domain.Entities.Users;
+using SoftDesign.Library.Domain.Interfaces.Repositories;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+
 namespace SoftDesign.Library.Services
 {
     public class AuthenticationService
     {
-        
+        private readonly IRepository<User> _userRepository;
+
+        public AuthenticationService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Result<string> Authenticate(string username, string password)
+        {
+            var user = _userRepository.Find(u => u.Username == username).FirstOrDefault();
+            if (user == null || !user.VerifyPassword(password))
+            {
+                return Result<string>.Failure("Invalid username or password");
+            }
+            var token = JwtService.GenerateToken(user.Username, "User");
+            return Result<string>.Success(token);
+        }
     }
 }
--- src/SoftDesign.Library.Services/BookService.cs
+++ src/SoftDesign.Library.Services/BookService.cs
@@ -1,7 +1,44 @@
+using SoftDesign.Library.Domain.Entities.Books;
+using SoftDesign.Library.Domain.Interfaces.Services;
+using System.Collections.Generic;
+using System;
+using SoftDesign.Library.Domain.Interfaces.Repositories;
+using System.Linq;
+using SoftDesign.Library.Domain.Entities.Users;
+
 namespace SoftDesign.Library.Services
 {
-    public class BookService
+    public class BookService : IBookService
     {
-        
+        private readonly IRepository<Book> _bookRepository;
+        private readonly IRepository<User> _userRepository;
+
+        public BookService(IRepository<Book> bookRepository, IRepository<User> userRepository)
+        {
+            _bookRepository = bookRepository;
+            _userRepository = userRepository;
+        }
+
+        public IEnumerable<Book> GetBooks(string search = null)
+        {
+            var query = _bookRepository.GetAll();
+
+            if (!string.IsNullOrEmpty(search))
+                query = query.Where(b => b.Title.Contains(search) || b.Author.Contains(search));
+
+            return query.ToList();
+        }
+
+        public void RentBook(int bookId, int userId, DateTime rentalDate, DateTime expectedReturnDate)
+        {
+            var book = _bookRepository.GetById(bookId);
+            var user = _userRepository.GetById(userId);
+
+            if (book == null || user == null)
+                throw new InvalidOperationException("Invalid book or user.");
+
+            user.RentBook(book, rentalDate, expectedReturnDate);
+            _bookRepository.Update(book);
+        }
     }
 }
